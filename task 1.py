560a1d7f7c8db3892e046541fa0fe29e2811c127
Saivaradeep <126085508+Saivaradeep@users.noreply.github.com>
2025-05-21 12:04:48 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,10 @@
+first=int(input("enter the First Number: "))
+second=int(input("enter the Second Number: "))
+a=first+second
+print("Addition: ",a)
+s=first-second
+print("Subtraction: ",s)
+m=first*second
+print("Multiplication: ",m)
+d=first/second
+print("division: ",d)
---
+++ task 2.py
@@ -0,0 +1,4 @@
+first = input("enter your first name: ")
+last = input("enter your last name: ")
+fullname = first + " " + last
+print("hello, " + fullname + "! welcome to the python program")
